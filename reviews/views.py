efa7200ecdc0e19795400710c75e64bf92de5485
s-omidikia <omidikiasaba74@gmail.com>
2026-06-06 15:51:24 +0330

    add search view with query parameter support

--- reviews/views.py
+++ reviews/views.py
@@ -8,3 +8,7 @@ from django.shortcuts import render
 def index(request):
     name = 'world'
     return render(request, "base.html", {'name': name})
+
+def book_search(request):
+    search_text = request.GET.get('search', "")
+    return render(request, "search_result.html", {'search_text': search_text})
