2740b83137562e321fd550d7ce87b2bcf83d94b8
s-omidikia <omidikiasaba74@gmail.com>
2026-06-13 02:00:20 +0330

    add Book model with title, publication_date, and isbn

--- reviews/models.py
+++ reviews/models.py
@@ -16,3 +16,18 @@ class Publisher(models.Model):
     email = models.EmailField(
         help_text="The Publisher's email address."
     )
+
+class Book(models.Model):
+    """A published book."""
+
+    title = models.CharField(
+        max_length=70,
+        help_text="The title of the book."
+    )
+    publication_date = models.DateField(
+        verbose_name="Date the book was published."
+    )
+    isbn = models.CharField(
+        max_length=20,
+        verbose_name="ISBN number of the book."
+    )
