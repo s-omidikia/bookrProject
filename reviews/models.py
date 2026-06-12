e7bf9e72bf30df30c7aa19a0199bcb22ade3bf2e
s-omidikia <omidikiasaba74@gmail.com>
2026-06-13 02:08:05 +0330

    add Contributor model with first_names, last_names, and email

--- reviews/models.py
+++ reviews/models.py
@@ -31,3 +31,19 @@ class Book(models.Model):
         max_length=20,
         verbose_name="ISBN number of the book."
     )
+
+class Contributor(models.Model):
+    """A contributor to a Book, e.g. author, editor,
+        co-author."""
+
+    first_names = models.CharField(
+        max_length=50,
+        help_text="The contributor's first name or names."
+    )
+    last_names = models.CharField(
+        max_length=50,
+        help_text="The contributor's last name or names."
+    )
+    email = models.EmailField(
+        help_text="The contact email for the contributor."
+    )
